3030f3437015a5eca61211fe8e6a2499d089b7ce
FooChuanTian <foochuantian@gmail.com>
2026-02-11 01:40:25 +0800

    implemented lives system

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -5,17 +5,26 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Collections;
 using Unity.VisualScripting;
+using System.Data;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
-    private bool isCyclingPath = false;
-    private bool isGameOver = false;
-    private float timeToRespawn;
     public TextMeshProUGUI WhichLaneText;
     public TextMeshProUGUI GameOverText;
     public Transform Checkpoint0;
-    private List<Transform> CheckpointList = new List<Transform>();
     public Transform Player;
+    public Transform FilledHeart1;
+    public Transform FilledHeart2;
+    public Transform FilledHeart3;
+    public Transform EmptyHeart1;
+    public Transform EmptyHeart2;
+    public Transform EmptyHeart3;
+    private bool isCyclingPath = false;
+    private bool isGameOver = false;
+    private float timeToRespawn;
+    
+    private List<Transform> CheckpointList = new List<Transform>();
+    private int lifeCount = 3;
 
     void Start()
     {
@@ -40,11 +49,16 @@ public class PlayerCollisionHandler : MonoBehaviour
         if (collision.gameObject.CompareTag("cyclist"))
         {
             Debug.Log("Collided with cyclist");
-            PlayerPositionManager positionManager = Player.gameObject.GetComponent<PlayerPositionManager>();
-            isGameOver = true;
-            timeToRespawn = 3f;
-            StartCoroutine(GameOver("Hit by cyclist"));
-            //positionManager.Teleport();
+            lifeCount--;
+            UpdateHearts(lifeCount);
+            if (lifeCount <= 0) 
+            {
+                PlayerPositionManager positionManager = Player.gameObject.GetComponent<PlayerPositionManager>();
+                isGameOver = true;
+                timeToRespawn = 3f;
+                StartCoroutine(GameOver("Hit by cyclist"));
+                //positionManager.Teleport();
+            }
         }
         else if (collision.gameObject.CompareTag("checkpoint"))
         {
@@ -77,6 +91,45 @@ public class PlayerCollisionHandler : MonoBehaviour
             isCyclingPath = false;
             WhichLaneText.text = "Pedestrian Lane Right";
             Debug.Log("On right pedestrian path!");
+        }
+    }
+
+    void UpdateHearts(int livesLeft)
+    {
+        switch (livesLeft)
+        {
+            case 0:
+                FilledHeart1.gameObject.SetActive(false);
+                FilledHeart2.gameObject.SetActive(false);
+                FilledHeart3.gameObject.SetActive(false);
+                EmptyHeart1.gameObject.SetActive(true);
+                EmptyHeart2.gameObject.SetActive(true);
+                EmptyHeart3.gameObject.SetActive(true);
+                break;
+            case 1:
+                FilledHeart1.gameObject.SetActive(false);
+                FilledHeart2.gameObject.SetActive(false);
+                FilledHeart3.gameObject.SetActive(true);
+                EmptyHeart1.gameObject.SetActive(true);
+                EmptyHeart2.gameObject.SetActive(true);
+                EmptyHeart3.gameObject.SetActive(false);
+                break;
+            case 2:
+                FilledHeart1.gameObject.SetActive(false);
+                FilledHeart2.gameObject.SetActive(true);
+                FilledHeart3.gameObject.SetActive(true);
+                EmptyHeart1.gameObject.SetActive(true);
+                EmptyHeart2.gameObject.SetActive(false);
+                EmptyHeart3.gameObject.SetActive(false);
+                break;
+            case 3:
+                FilledHeart1.gameObject.SetActive(true);
+                FilledHeart2.gameObject.SetActive(true);
+                FilledHeart3.gameObject.SetActive(true);
+                EmptyHeart1.gameObject.SetActive(false);
+                EmptyHeart2.gameObject.SetActive(false);
+                EmptyHeart3.gameObject.SetActive(false);
+                break;
         }
     }
 

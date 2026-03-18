7ba917aa77720fab8d8ae8799e2f228b95638097
FooChuanTian <foochuantian@gmail.com>
2026-03-18 17:13:27 +0800

    bike bell changes
    -1s delay added after bell is heard by NPCs
    -If NPC hears bike bell, an exclamation mark appears above them

--- Assets/Scripts/BikeBellBehaviour.cs
+++ Assets/Scripts/BikeBellBehaviour.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +16,7 @@ public class BikeBellBehaviour : MonoBehaviour
         Debug.Log("Bike Bell Range");
         if (Keyboard.current.zKey.wasPressedThisFrame && (other.gameObject.CompareTag("pedestrian") || other.gameObject.CompareTag("cyclist")))
         {
+            other.gameObject.GetComponent<SetTextAcive>().SetTextActive();
             Rigidbody rb_other = other.gameObject.GetComponent<Rigidbody>();
             //Vector3 pos_difference = other.gameObject.transform.position - transform.position;
             Vector3 pos_difference = new Vector3(0, 0, other.gameObject.transform.position.z - transform.position.z);
@@ -21,11 +24,12 @@ public class BikeBellBehaviour : MonoBehaviour
             {
                 pos_difference.z = 4;
             }
-            rb_other.AddForce(pos_difference*50, ForceMode.Acceleration);
+            //rb_other.AddForce(pos_difference*30, ForceMode.Acceleration);
+            StartCoroutine(MoveNPC(rb_other, pos_difference));
         }
     }
 
-    
+
     void Update()
     {
         if (Keyboard.current.zKey.wasPressedThisFrame)
@@ -33,5 +37,11 @@ public class BikeBellBehaviour : MonoBehaviour
             Debug.Log("Bike bell");
             BellSound.Play();
         }
+    }
+
+    private IEnumerator MoveNPC(Rigidbody rb, Vector3 pos_difference)
+    {
+        yield return new WaitForSeconds(1f);
+        rb.AddForce(pos_difference*30, ForceMode.Acceleration);
     }
 }

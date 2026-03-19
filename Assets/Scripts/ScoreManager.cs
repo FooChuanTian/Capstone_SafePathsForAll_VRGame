0671ba611ba03fc148ec10b223af7e77f6643110
FooChuanTian <foochuantian@gmail.com>
2026-03-20 00:18:24 +0800

    number of times phones is opened measured

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,28 @@
+using System.Data;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ScoreManager : MonoBehaviour
 {
     public int SecondsOnCorrectLane;
     public int SecondsOnWrongLane;
+    public int PhoneOpened;
     public string PlayerType;
     public TextMeshProUGUI CorrectLaneDebug;
     public TextMeshProUGUI WrongLaneDebug;
+    public GameObject PhoneObject;
     private string[] CorrectLanes;
     private string CurrentLane;
+
     private string[] AllLanes = {"pedestrian_lane_left", "pedestrian_lane_right", "cycling_lane_left", "cycling_lane_right"};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SecondsOnCorrectLane = 0;
         SecondsOnWrongLane = 0;
+        PhoneOpened = 0;
 
         if (PlayerType == "pedestrian")
         {
@@ -53,6 +59,17 @@ public class ScoreManager : MonoBehaviour
                 WrongLaneDebug.text = "Wrong Lane: " + SecondsOnWrongLane;
             }
             CurrentLane = null;
+        }
+    }
+
+    void Update()
+    {
+        if (PhoneObject != null)
+        {
+            if (Keyboard.current.xKey.wasPressedThisFrame && PhoneObject.activeSelf)
+            {
+                PhoneOpened++;   
+            }
         }
     }
 }

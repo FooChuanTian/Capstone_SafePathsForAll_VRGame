6f6400bb5300eec8fc01d65d96fccfdad0001e4e
FooChuanTian <foochuantian@gmail.com>
2026-03-20 21:52:46 +0800

    trying to fix speed indicator

--- Assets/Scripts/SpeedIndicator.cs
+++ Assets/Scripts/SpeedIndicator.cs
@@ -1,14 +1,30 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpeedIndicator : MonoBehaviour
 {
     public TextMeshProUGUI SpeedText;
     public Rigidbody rb;
-void Update()
+    public Transform tf;
+    private Vector3 PreviousPosition;
+    private Vector3 CurrentPosition;
+    private Vector3 PositionDelta;
+    private float SmoothSpeed;
+    void Start()
     {
-        float velocity = rb.linearVelocity.magnitude;
-        SpeedText.text = "Speed: " + (int) velocity;
+        PreviousPosition = tf.position;
+        SmoothSpeed = 0;
+    }
+    void Update()
+    {
+        PositionDelta = rb.linearVelocity;
+        PositionDelta = tf.position - PreviousPosition;
+        float rawspeed = PositionDelta.magnitude / Time.deltaTime;
+        SmoothSpeed = Mathf.Lerp(SmoothSpeed, rawspeed, Time.deltaTime*2f);
+        SpeedText.text = "Speed: " + Math.Round(SmoothSpeed);
+        //SpeedText.text = "Speed: " + (int) ((CurrentPosition - PreviousPosition).magnitude / Time.deltaTime);
+        PreviousPosition = tf.position;
     }
 }

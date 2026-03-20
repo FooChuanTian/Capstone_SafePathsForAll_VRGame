3078cbefa0bb87ad8d5db90a66f3047e2ff9a567
FooChuanTian <foochuantian@gmail.com>
2026-03-20 17:05:00 +0800

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -38,7 +38,7 @@ public class ScoreManager : MonoBehaviour
 
     void SecondUpdate()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 0.2f);
         foreach (var hitCollider in hitColliders) {
             if (AllLanes.Contains(hitCollider.gameObject.tag))
             {

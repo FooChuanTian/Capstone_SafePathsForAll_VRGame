12574c6a08372c2a70eff69d4bd7864e0c559a5b
FooChuanTian <foochuantian@gmail.com>
2026-03-25 15:31:19 +0800

    fixing scoreboard in VR

--- Assets/Scripts/OpenScoreBoard.cs
+++ Assets/Scripts/OpenScoreBoard.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARSubsystems;
 
 public class OpenScoreBoard : MonoBehaviour
 {
     public Transform ScoreBoardObject;
+    public Transform ScoreBoardVR;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Time.timeScale = 0;
-            ScoreBoardObject.gameObject.SetActive(true);
+            if (XRSettings.enabled)
+            {
+                ScoreBoardVR.gameObject.SetActive(true);
+            }
+            else 
+            {
+                ScoreBoardObject.gameObject.SetActive(true);
+            }
         }
     }
 }

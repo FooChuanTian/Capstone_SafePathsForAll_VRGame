92b7e106f20fdee07ba62a754765bfd135c30c3a
FooChuanTian <foochuantian@gmail.com>
2026-03-25 18:30:59 +0800

    Update VRPlayerSelect.cs

--- Assets/Scripts/VRPlayerSelect.cs
+++ Assets/Scripts/VRPlayerSelect.cs
@@ -8,11 +8,11 @@ public class VRPlayerSelect : MonoBehaviour
     {
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
-            SceneManager.LoadScene("CyclistSimulationScene");
+            SceneManager.LoadScene("PedestrianSimulationScene");
         }
         else if (OVRInput.GetDown(OVRInput.Button.Four))
         {
-            SceneManager.LoadScene("PedestrianSimulationScene");
+            SceneManager.LoadScene("CyclistSimulationScene");
         }
     }
 }

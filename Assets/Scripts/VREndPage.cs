261501c6b85d911428a639c78c470bfc610c27ab
FooChuanTian <foochuantian@gmail.com>
2026-03-26 23:16:20 +0800

    mapping vr buttons to pedestrian endpage

---
+++ Assets/Scripts/VREndPage.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class VREndPage : MonoBehaviour
+{
+    private bool acceptInput = false;
+    void Start()
+    {
+        StartCoroutine(DelayedStart());
+    }
+    void Update()
+    {
+        StartPedestrianLesson startPedestrian = new StartPedestrianLesson();
+        if (OVRInput.GetDown(OVRInput.Button.Three) && acceptInput)
+        {
+            startPedestrian.StartLesson();
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.Four) && acceptInput)
+        {
+            startPedestrian.RepeatLesson();
+        }
+    }
+    private IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(2f);
+        acceptInput = true;
+    }
+
+}

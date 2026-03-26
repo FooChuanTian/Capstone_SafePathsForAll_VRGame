040126a8af2c65091a7ff96b77d6b4286d40d17e
FooChuanTian <foochuantian@gmail.com>
2026-03-26 21:01:34 +0800

    fixing checkpoints in vr

--- Assets/Scenes/Atsadet/scripts/Checkpoint_0.cs
+++ Assets/Scenes/Atsadet/scripts/Checkpoint_0.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Checkpoint_0 : MonoBehaviour
 {   
     public Rigidbody rb;
+    public Rigidbody rb_vr;
+    private Rigidbody rb_curr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (XRSettings.enabled)
+        {
+            rb_curr = rb_vr;
+        }
+        else
+        {
+            rb_curr = rb;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // The wall only moves if the player is further ahead than the wall
-        if (rb.position.x < transform.position.x - 10f)
+        if (rb_curr.position.x < transform.position.x - 10f)
         {
             // transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, rb.position.z);
             transform.position = transform.position - new Vector3(1f, 0, 0);

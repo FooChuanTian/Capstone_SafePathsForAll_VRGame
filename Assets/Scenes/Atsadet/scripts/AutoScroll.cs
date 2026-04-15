5561e93ecedbaef328c44bbd7dd847f479638b1a
scadettt <hoatsadet@gmail.com>
2026-04-15 18:51:30 +0800

    add func to restart game

--- Assets/Scenes/Atsadet/scripts/AutoScroll.cs
+++ Assets/Scenes/Atsadet/scripts/AutoScroll.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class AutoScroll : MonoBehaviour
 {
     public float scrollSpeed = 50f;
     private RectTransform rectTransform;
+    private bool isStopped = false;
 
     void Start()
     {
@@ -16,9 +19,25 @@ public class AutoScroll : MonoBehaviour
         rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
         
         // Optional: Loop back to bottom if it goes too high
-        if (rectTransform.anchoredPosition.y > -1700f) 
+        // if (rectTransform.anchoredPosition.y > -1700f) 
+        // {
+        //     rectTransform.anchoredPosition = new Vector2(0, -3563f);
+        // }
+        if (rectTransform.anchoredPosition.y > -2000f) 
         {
-            rectTransform.anchoredPosition = new Vector2(0, -3563f);
+            scrollSpeed = 0;
+            isStopped = true;
+        }
+
+        if (isStopped && OVRInput.GetDown(OVRInput.Button.Three))
+        {   
+            GameObject[] persistentObjects = GameObject.FindGameObjectsWithTag("PersistentObj");
+            foreach (GameObject obj in persistentObjects)
+            {
+                Destroy(obj);
+                Debug.Log("Destroyed " + obj.name);
+            }
+            SceneManager.LoadScene("PlayerSelectionScene");
         }
     }
 }
